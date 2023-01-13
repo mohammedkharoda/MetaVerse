1e43665eced774052bc973a774317bd7c788d10c
Mohammed Kharoda <kharodawalam@gmail.com>
2023-01-13 22:24:30 +0530

    Explore page Inital Started

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { Footer, Navbar } from "../components";
 import {
   About,
@@ -16,22 +18,19 @@ const Page = () => (
     <Hero />
     <div className="relative">
       <About />
-      <div className="gradient-03 z-0">
-        <Explore />
-      </div>
+      <div className="gradient-03 z-0" />
+      <Explore />
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-03 z-0">
-        <WhatsNew />
-      </div>
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
     <World />
     <div className="relative">
       <Insights />
-      <div className="gradient-04 z-0">
-        <Feedback />
-      </div>
+      <div className="gradient-04 z-0" />
+      <Feedback />
     </div>
     <Footer />
   </div>
