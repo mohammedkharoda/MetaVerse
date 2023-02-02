999492643b989d31fa5217cda4e2848a6138f702
mohammed kharoda <kharodawalam@gmail.com>
2023-02-02 22:04:11 +0530

    Final Fixing an push �

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { motion } from "framer-motion";
+
 import styles from "../styles";
-import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
-import { NewFeatures, TitleText, TypingText } from "../components";
 import { newFeatures } from "../constants";
+import { NewFeatures, TitleText, TypingText } from "../components";
+import { planetVariants, staggerContainer, fadeIn } from "../utils/motion";
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -12,21 +13,22 @@ const WhatsNew = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
-      className={`${styles.innerWidth} flex mx-auto lg:flex-row flex-col gap-8`}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
         variants={fadeIn("right", "tween", 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col"
+        className="flex-[0.95] flex justify-center flex-col"
       >
-        <TypingText title="|What's New" />
-        <TitleText title={<>What's new about Metaversus</>} />
-        <div className="mt-[48px] flex justify-between gap-[24px]">
-          {newFeatures.map((features) => {
-            return <NewFeatures key={features.title} {...features} />;
-          })}
+        <TypingText title="| Whats new?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
         </div>
       </motion.div>
+
       <motion.div
         variants={planetVariants("right")}
         className={`flex-1 ${styles.flexCenter}`}
--- sections/World.jsx
+++ sections/World.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import { motion } from "framer-motion";
+
 import styles from "../styles";
-import { staggerContainer, fadeIn } from "../utils/motion";
 import { TitleText, TypingText } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const World = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -12,43 +12,36 @@ const World = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
-      className={`${styles.innerWidth} flex mx-auto flex-col`}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="|People on the World" textStyles="text-center" />
+      <TypingText title="| People on the World" textStyles="text-center" />
       <TitleText
         title={
           <>
-            Track Friends Around and Invite them to play together in the same
-            world
+            Track friends around you and invite them to play together in the
+            same world
           </>
         }
         textStyles="text-center"
       />
-      {/* motion-div */}
+
       <motion.div
         variants={fadeIn("up", "tween", 0.3, 1)}
-        className="relative mt-[68px] flex w-full h0 [550px]"
+        className="relative mt-[68px] flex w-full h-[550px]"
       >
         <img src="/map.png" alt="map" className="w-full h-full object-cover" />
-        {/* ---- guy-1 ---- */}
-        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-01.png" alt="people" className="w-full h-full" />
         </div>
-        {/* ---- guy-2 ----  */}
-        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
-          <img src="people-02.png" alt="people" className="w-full h-full" />
+
+        <div className="absolute top-10 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src="/people-02.png" alt="people" className="w-full h-full" />
         </div>
-        {/* ---- guy-3 ---- */}
-        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5D6680]">
           <img src="people-03.png" alt="people" className="w-full h-full" />
-        </div>
-        {/* ---- guy-4 ---- */}
-        <div className="absolute top-2/4 left-[20%] w-[190px] h-auto p-[6px] rounded-[15px] bg-[#5d6680]">
-          <img src="mask-group.png" alt="people" className="w-full h-full" />
-          <h1 className="absolute left-[25%] bottom-[3rem] text-white">
-            + 264 has joined
-          </h1>
         </div>
       </motion.div>
     </motion.div>
